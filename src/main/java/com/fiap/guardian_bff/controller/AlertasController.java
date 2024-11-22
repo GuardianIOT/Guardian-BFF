c0b5bb666ebe55d51b738f4b56167bb28b28f651
kayqueln <kayqueln@gmail.com>
2024-11-22 14:26:28 -0300

    devops

--- src/main/java/com/fiap/guardian_bff/controller/AlertasController.java
+++ src/main/java/com/fiap/guardian_bff/controller/AlertasController.java
@@ -1,5 +1,6 @@
 package com.fiap.guardian_bff.controller;
 
+import com.fiap.guardian_bff.dto.alertas.ListarAlertasDTO;
 import com.fiap.guardian_bff.dto.util.ErrorMessageDTO;
 import com.fiap.guardian_bff.model.Alerta;
 import com.fiap.guardian_bff.service.AlertasService;
@@ -10,11 +11,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Tag(name = "Alertas", description = "API para realizar operações relacionadas as alertas geradas pelos sensores")
@@ -25,25 +28,37 @@ public class AlertasController {
     @Autowired
     private AlertasService alertasService;
 
-    @Operation(summary = "Lista o histórico de alertas associados a companhia do usuário", responses = {
+    @Operation(summary = "Lista o histórico de alertas associados à companhia do usuário", responses = {
             @ApiResponse(responseCode = "200", description = "Sucesso",
                     content = @Content(array = @ArraySchema(schema = @Schema(implementation = Alerta.class)))),
             @ApiResponse(responseCode = "204", description = "No Content")})
     @GetMapping
-    public ResponseEntity listarAlertas(@AuthenticationPrincipal String emailUsuario) {
+    public ResponseEntity listarAlertas(@AuthenticationPrincipal String emailUsuario,
+                                        @RequestParam(defaultValue = "0") int page,
+                                        @RequestParam(defaultValue = "10") int size) {
         try {
-            return ResponseEntity.ok(alertasService.listarAlertas(emailUsuario));
+            Page<Alerta> alertasPaginados = alertasService.listarAlertas(emailUsuario, page, size);
+            if (alertasPaginados.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.ok(alertasPaginados);
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(new ErrorMessageDTO("Ocorreu um erro inesperado"));
         }
     }
 
-    @Operation(summary = "Lista todo o histórico de alerta", responses = {
+    @Operation(summary = "Lista todo o histórico de alertas", responses = {
             @ApiResponse(responseCode = "200", description = "Sucesso",
                     content = @Content(array = @ArraySchema(schema = @Schema(implementation = Alerta.class)))),
             @ApiResponse(responseCode = "204", description = "No Content")})
     @GetMapping("listar-todos")
-    public ResponseEntity listarTodasAlertas() {
-        return ResponseEntity.ok(alertasService.listarAlertas());
+    public ResponseEntity listarTodasAlertas(@RequestParam(defaultValue = "0") int page,
+                                             @RequestParam(defaultValue = "10") int size) {
+        Page<ListarAlertasDTO> alertasPaginados = alertasService.listarAlertas(page, size);
+        if (alertasPaginados.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(alertasPaginados);
     }
+
 }
--- src/main/java/com/fiap/guardian_bff/repository/AlertasRepository.java
+++ src/main/java/com/fiap/guardian_bff/repository/AlertasRepository.java
@@ -2,12 +2,16 @@ package com.fiap.guardian_bff.repository;
 
 import com.fiap.guardian_bff.model.Alerta;
 import com.fiap.guardian_bff.model.Companhia;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AlertasRepository extends JpaRepository<Alerta, Long> {
-    List<Alerta> findAllByCompanhia(Companhia companhia);
+
+    Page<Alerta> findAllByCompanhia(Companhia companhia, Pageable pageable);
+
+    @Override
+    Page<Alerta> findAll(Pageable pageable);
 }
